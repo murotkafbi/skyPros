c4b4059bc29de8827f598a662344a092e152e6ba
GURitch <guritch1@mail.ru>
2023-05-02 12:26:52 +0300

    Initial commit

---
+++ src/test/java/com/skypro/javaind/demoshoppingcart/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.javaind.demoshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
